b0756a3ab9690ca9f4f8c86b61ec3bbdfddce935
Antoine Chevalier <53263615+SeyBoo@users.noreply.github.com>
2023-03-04 15:11:29 +0100

    integrate design

---
+++ src/common/assets/barista/index.tsx
@@ -0,0 +1,8 @@
+import Lottie from "lottie-react";
+import BaristaAnimation from "./baristaAnimation.json";
+
+export const Barista = () => {
+    return (
+        <Lottie animationData={BaristaAnimation}/>
+    )
+}
---
+++ src/common/utils/verifyEmail.ts
@@ -0,0 +1,7 @@
+export const ValidateEmail = (email: string) => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        return true;
+    }
+    return false;
+
+}
