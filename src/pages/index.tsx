a3f4a52f85d508bd01dabc96d2eb4740cba8ff77
Antoine Chevalier <53263615+SeyBoo@users.noreply.github.com>
2023-03-05 12:05:18 +0100

    correct overflow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,38 +42,42 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <main className="w-[90%] m-auto py-6 flex flex-col gap-12 max-w-[1660px] m-auto">
-                <header className="flex items-center">
-                    <Image src={Logo} alt="Waycup-logo"/>
-                </header>
-                <div className="md:grid md:grid-cols-2 gap-4 lg:items-center xl:max-w-[90%] m-auto">
-                    <div className="flex flex-col gap-4 lg:gap-8 items-start">
-                        <span className="bg-[#FF5E5D] text-white py-0.5 px-6 rounded-full">For businesses</span>
-                        <h1 className="font-bold text-xl md:text-3xl leading-7 text-gray-700">
-                            Are you an independent coffee shop? We’re building
-                            <HighlightedText>digital tools</HighlightedText> for you - and they’re <HighlightedText>
-                            FREE!</HighlightedText>
-                        </h1>
-                        <p className="font-lights leading-6 text-[#555555] xl:text-xl">How can you guarantee that your
-                            customers
-                            will
-                            come back to you once they’ve left your store?
-                            We’re building digital tools that will give you that guarantee and so much more. We know the
-                            struggles of running an independent business so we’re giving you the digital tools to
-                            compete
-                            with major coffee chains and it won’t cost you a thing.</p>
-                        <div className="w-full hidden md:block md:mt-4">
-                            {handleRenderForm()}
+            <div className="w-screen overflow-hidden">
+                <main className="w-[90%] m-auto py-6 flex flex-col gap-12 max-w-[1660px] m-auto">
+                    <header className="flex items-center">
+                        <Image src={Logo} alt="Waycup-logo"/>
+                    </header>
+                    <div className="md:grid md:grid-cols-2 gap-4 lg:items-center xl:max-w-[90%] m-auto">
+                        <div className="flex flex-col gap-4 lg:gap-8 items-start">
+                            <span className="bg-[#FF5E5D] text-white py-0.5 px-6 rounded-full">For businesses</span>
+                            <h1 className="font-bold text-xl md:text-3xl leading-7 text-gray-700">
+                                Are you an independent coffee shop? We’re building
+                                <HighlightedText>digital tools</HighlightedText> for you - and they’re <HighlightedText>
+                                FREE!</HighlightedText>
+                            </h1>
+                            <p className="font-lights leading-6 text-[#555555] xl:text-xl">How can you guarantee that
+                                your
+                                customers
+                                will
+                                come back to you once they’ve left your store?
+                                We’re building digital tools that will give you that guarantee and so much more. We know
+                                the
+                                struggles of running an independent business so we’re giving you the digital tools to
+                                compete
+                                with major coffee chains and it won’t cost you a thing.</p>
+                            <div className="w-full hidden md:block md:mt-4">
+                                {handleRenderForm()}
+                            </div>
+                        </div>
+                        <div className="md:-mr-96">
+                            <Barista/>
                         </div>
                     </div>
-                    <div className="md:-mr-96">
-                        <Barista/>
+                    <div className="md:hidden">
+                        {handleRenderForm()}
                     </div>
-                </div>
-                <div className="md:hidden">
-                    {handleRenderForm()}
-                </div>
-            </main>
+                </main>
+            </div>
         </>
     )
 }
